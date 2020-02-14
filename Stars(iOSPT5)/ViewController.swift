91f6a88f93ee7b0236fc77f7c670131c1952a788
David Williams <burmesedragon@att.net>
2020-02-13 17:16:00 -0800

    Initial Commit

---
+++ Stars(iOSPT5)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars(iOSPT5)
+//
+//  Created by David Williams on 2/13/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
